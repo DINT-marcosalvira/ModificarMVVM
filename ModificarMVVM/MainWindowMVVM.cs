d9e24bcde812f46d7bf6a7248f560c00338a1382
Marcos Alvira Romero <marcos.alvira@iesdoctorbalmis.com>
2020-01-15 14:09:07 +0100

    Agregar archivos de proyecto.

---
+++ ModificarMVVM/MainWindowMVVM.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModificarMVVM
+{
+    class MainWindowMVVM : INotifyPropertyChanged
+    {
+        private BDMarcosEntities contexto;
+
+
+        public MainWindowMVVM()
+        {
+            contexto = new BDMarcosEntities();
+            contexto.CLIENTES.Load();
+        }
+
+        public void Modificar()
+        {
+            contexto.SaveChanges();
+        }
+
+        public ObservableCollection<CLIENTE> getClientes()
+        {
+            return contexto.CLIENTES.Local;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
